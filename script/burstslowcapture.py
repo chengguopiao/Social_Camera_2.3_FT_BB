477905e15bb787091a06fa58b67acacdf9b89b3d
chengguopiao <chengguo.piao@borqs.com>
2014-08-26 16:11:44 +0800

    Update burstslowcapture.py

--- script/burstslowcapture.py
+++ script/burstslowcapture.py
@@ -30,7 +30,7 @@ class CameraTest(unittest.TestCase):
         time.sleep(2)
         if d(text = 'OK').wait.exists(timeout = 3000):
             d(text = 'OK').click.wait()        
-        sm.switchCaptureMode('burstslow')
+        sm.switchCaptureMode('Burst','SLOW')
 
     def tearDown(self):
         super(CameraTest,self).tearDown()
