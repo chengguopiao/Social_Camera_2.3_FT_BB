9c2017a300493e5227e092057b0d4272cb603925
chengguopiao <chengguo.piao@borqs.com>
2014-09-29 14:08:10 +0800

    Update hdrcapture.py

--- script/hdrcapture.py
+++ script/hdrcapture.py
@@ -70,8 +70,6 @@ class CameraTest(unittest.TestCase):
         #Refresh media after delete files
         a.cmd('refresh','/sdcard/DCIM/*')
         #Launch social camera
-        if d(text = 'Skip').wait.exists(timeout = 3000):
-            d(text = 'Skip').click.wait()          
         self._launchCamera()
         sm.switchCaptureMode('Single','HDR')
 
@@ -198,6 +196,8 @@ class CameraTest(unittest.TestCase):
     def _launchCamera(self):
         d.start_activity(component = ACTIVITY_NAME)
         #When it is the first time to launch camera there will be a dialog to ask user 'remember location', so need to check
+        if d(text = 'Skip').wait.exists(timeout = 3000):
+            d(text = 'Skip').click.wait()          
         try:
             assert d(text = 'OK').wait.exists(timeout = 2000)
             d(text = 'OK').click.wait()
