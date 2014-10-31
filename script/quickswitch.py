d4aed2f888c556cbadd42f7a4e68febe816df27a
chengguopiao <chengguo.piao@borqs.com>
2014-10-31 10:12:39 +0800

    Update quickswitch.py

--- script/quickswitch.py
+++ script/quickswitch.py
@@ -34,8 +34,8 @@ class CameraTest(unittest.TestCase):
         #Step 1. Launch single capture activity
         a.cmd('launch','com.intel.camera22/.Camera')
         time.sleep(2)
-        if  d(text = 'OK').wait.exists(timeout = 3000):
-            d(text = 'OK').click.wait()
+        if  d(text = 'Yes').wait.exists(timeout = 3000):
+            d(text = 'Yes').click.wait()
         if d(text = 'Skip').wait.exists(timeout = 3000):
             d(text = 'Skip').click.wait() 
 
